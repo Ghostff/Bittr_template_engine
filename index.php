8e5542c75c3d005db98313701a03a790514f7c2f
ghostff <frankchris@hotmail.com>
2016-07-02 20:28:15 -0500

    Edited index'

--- index.php
+++ index.php
@@ -3,14 +3,6 @@ require_once 'Bittr' . DIRECTORY_SEPARATOR . '/init.php';
 $bittr = new Bittr();
 $bittr->setTemplatePath(__DIR__ . '/Bittr/Templates');
 
-/*$m['user'] = array(
-					array('fname' => 'chrys', 'lname' => array('named' => 'chiderah')), 
-				    array('fname' => 'chiderah', 'lname' => 'uwgu')
-			);
-
-
-$bittr->set($fname = 'chrys', $lname = 'ugwu', $title = 'Bitter Template Engine', $m);
-$bittr->assign('name', 'chidex');*/
 $bittr->assign('users', array('a' => 'aba', 'i' => 'imo', 'j' => 'jigawa'));
 
 
