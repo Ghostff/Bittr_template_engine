eb40b33d91180501d3be461213542a142c06186b
ghostff <frankchris@hotmail.com>
2016-11-25 19:27:33 -0600

    fixed nexted statement doom

--- src/Compiler/Template.php
+++ src/Compiler/Template.php
@@ -28,72 +28,88 @@ class Template
     {
         $this->lines = $template_array;
         static::$name = $name;
-		
-		$this->req_ptn = '/' . static::$open . '\s*(req|inc) \'(.*?)\'\s*' . static::$close .'/';
-		$this->var_ptn = '/' . static::$open . '\s*(\w+)([\.\w]*|(\s*<(.*?)>)*|\s*=\s*\'(.*?)\')\s*' . static::$close .'/';
-		
-		$this->if_ptn = '/' . static::$open . '\s*' . Config::$statement['if'] . ' (\w+)\s*' . static::$close .'/';
-		$this->endif_ptn = '/' . static::$open . '\s*' . Config::$statement['endif'] . '\s*' . static::$close . '/';
+        
+        $this->req_ptn = '/' . static::$open . '\s*(req|inc) \'(.*?)\'\s*' . static::$close .'/';
+        $this->var_ptn = '/' . static::$open . '\s*(\w+)([\.\w]*|(\s*<(.*?)>)*|\s*=\s*\'(.*?)\')\s*' . static::$close .'/';
+        
+        $this->if_ptn = '/' . static::$open . '\s*' . Config::$statement['if'] . ' (\w+)\s*' . static::$close .'/';
+        $this->endif_ptn = '/' . static::$open . '\s*' . Config::$statement['endif'] . '\s*' . static::$close . '/';
         
     }
-	
-	private function checkfileSystem($strings, &$line_content, $line)
-	{
-		if (preg_match_all($this->req_ptn, $strings, $matched)) {
-			\Sandbox\GetFile::evaluate($matched, $line_content, $line, static::$name);
-		}
-	}
-	
-	private function checkVaribales($strings, &$line_content, $line)
-	{
-		reExec: {
-			if (preg_match_all($this->var_ptn, $strings, $matched)) {
-				$varible = \Sandbox\Variable::evaluate($matched, $line_content, $line, static::$name);
-				if ($varible == 'reExec') {
-					$strings = $line_content[$line];
-					goto reExec;    
-				}
-			}
-		}
-	}
-	
-	private function checkStatement($strings, &$line_content, $line, &$if_attribute, &$is_if_body)
-	{
-		if (preg_match($this->if_ptn, $strings, $matched)) {
-			$if_attribute[] = array($line, $matched);
-			$is_if_body = true;
-			return true;
-		}
-		elseif (preg_match($this->endif_ptn, $strings, $matched)) {
-			$if_attribute[] = array($line, $matched);
-			$is_if_body = false;
-			\Sandbox\Statement::evaluate($if_attribute, $line_content);
-			var_dump(111);
-			return true;
-		}
-		elseif ($is_if_body) {
-			$if_attribute[] = array($line, $strings);
-			return true;
-		}
-		return false;
-	}
+    
+    private function checkfileSystem($strings, &$line_content, $line)
+    {
+        if (preg_match_all($this->req_ptn, $strings, $matched)) {
+            \Sandbox\GetFile::evaluate($matched, $line_content, $line, static::$name);
+        }
+    }
+    
+    private function checkVaribales($strings, &$line_content, $line)
+    {
+        reExec: {
+            if (preg_match_all($this->var_ptn, $strings, $matched)) {
+                $varible = \Sandbox\Variable::evaluate($matched, $line_content, $line, static::$name);
+                if ($varible == 'reExec') {
+                    $strings = $line_content[$line];
+                    goto reExec;    
+                }
+            }
+        }
+    }
+    
+    private function checkStatement($strings, &$line_content, $line, &$if_attribute, &$is_if_body, &$if_count)
+    {
+        if (preg_match($this->if_ptn, $strings, $matched)) {
+            
+            
+            if ($if_count > 0) {
+                $if_attribute[] = array($line, $matched[0]);
+            }
+            else {
+                $if_attribute[] = array($line, $matched);
+            }
+            $if_count++;
+            $is_if_body = true;
+            return true;
+        }
+        elseif (preg_match($this->endif_ptn, $strings, $matched)) {
+            if ($if_count == 1) {
+                $if_attribute[] = array($line, $matched);
+                $is_if_body = false;
+                
+                \Sandbox\Statement::evaluate($if_attribute, $line_content);
+            }
+            else {
+                $if_attribute[] = array($line, $matched[0]);
+            }
+            $if_count--;
+            return true;
+            
+        }
+        elseif ($is_if_body) {
+            $if_attribute[] = array($line, $strings);
+            return true;
+        }
+        return false;
+    }
     
     
     private function getSyntax($line_content, $line_number = null)
     {
         $new_String = null;
         
-		$if_attribute = array();
-		$is_if_body = false;
+        $if_attribute = array();
+        $is_if_body = false;
+        $if_count = 0;
 
         foreach ($line_content as $line => $strings) {
             
-			static::$line_number = ($line_number) ? $line_number : $line;
-			if ($this->checkStatement($strings, $line_content, $line, $if_attribute, $is_if_body)) {
-				continue;
-			}
-			$this->checkfileSystem($strings, $line_content, $line);
-			$this->checkVaribales($strings, $line_content, $line);
+            static::$line_number = ($line_number) ? $line_number : $line;
+            if ($this->checkStatement($strings, $line_content, $line, $if_attribute, $is_if_body, $if_count)) {
+                continue;
+            }
+            $this->checkfileSystem($strings, $line_content, $line);
+            $this->checkVaribales($strings, $line_content, $line);
 
         }
         return $line_content;
--- src/Sandbox/Statement.php
+++ src/Sandbox/Statement.php
@@ -6,34 +6,46 @@ class Statement
 {
     public static function evaluate($type, &$line_string)
     {
-		$attributes = \Compiler\Bittr::$attributes;
-		
+        $attributes = \Compiler\Bittr::$attributes;
+
+        //get statement identifier
         $stement = $type[0][1][1];
-		$begin = $type[0][0];
-		$end = end($type);
-		
-		$end = end($type);
-		for ($i = $begin; $i <= $end[0]; $i++) {
-			unset($line_string[$i]);	
-		}
-		
-		if (array_key_exists($stement, $attributes)) {
+        //get the line number of where statement began @{ if (identifer) }
+        $begin = $type[0][0];
+        //get the line number og the end of statemen @{ enf }
+        $end = end($type);
+        
+        //prevent the statement from displaying:
+        //clear out the line that contains the current statemne from @{ if .. to enf } from line buffer
+        for ($i = $begin; $i <= $end[0]; $i++) {
+            unset($line_string[$i]);    
+        }
+        //check if key exists in attribute
+        if (array_key_exists($stement, $attributes)) {
+            
+            $new_if = $type;
+            //remove the first(@{ if .. }) and last (@{ enf }) array element which mean all left will be the body of the statement 
+            array_shift($new_if);
+            array_pop($new_if);
+            
+            $new_string = array();
+            //set the line number of the statement body to the key of each statement body array
+            //more like we want it to seem like we are trying to compile with a new file
+            foreach ($new_if as $key => $values) {
+                $new_string[$values[0]] = $values[1];
+            }
 			
-			$new_if = $type;
-			array_shift($new_if);
-			array_pop($new_if);
-			$incr = 1;
-			
-			$new_string = array();
-			foreach ($new_if as $key => $values) {
-				$new_string[$values[0]] = $values[1];
-			}
-			
-			$template =  new \Compiler\Template($new_string, \Compiler\Template::$name);
-			$new_string = array_map('trim', $template->compile());
-			$line_string += $new_string;
-			ksort($line_string);	
-			
-		}
+            //compile the new string
+            $template =  new \Compiler\Template($new_string, \Compiler\Template::$name);
+            $new_string = array_map('trim', $template->compile());
+            //push back the compiled string to line buffer, since they keys has been unset, at line 21, we want to push back the key
+            //thus setting the new keys to the content of compiled statment
+            $line_string += $new_string;
+            //in most casses the key will be added to the last of the line buffer, making the compiled statement to fall at the last line 
+            //when outputed.
+            ksort($line_string);
+            //puts they key back to the original line before statement compilation
+            
+        }
     }
 }
