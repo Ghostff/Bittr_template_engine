24ba32b193bc2199c73021ed506b3ce402b39aa9
ghostff <frankchris@hotmail.com>
2016-11-28 12:24:14 -0600

    added label excape removal

--- src/Sandbox/Label.php
+++ src/Sandbox/Label.php
@@ -6,8 +6,14 @@ class Label
 {
     public static function evaluate($type, &$line_string, $line)
     {
-        if (trim($type[1]) != \Config\Config::$allow_labe_display) {
+		$is_excape = trim($type[1]);
+        if ($is_excape != \Config\Config::$allow_labe_display) {
             $line_string[$line] = null;
         }
+		else {
+			//remove the (!) at the end of the label
+			$new_label = preg_replace('/\s*' . \Config\Config::$allow_labe_display .'(\s*])$/', '$1', $type[0]);
+			$line_string[$line] = $new_label;
+		}
     }
 }
