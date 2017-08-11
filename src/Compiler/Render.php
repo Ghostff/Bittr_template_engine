eb29744ddcf884ae11b04d063734c4dc7c6b256f
chiderah ugwu <chiderah@medology.com>
2017-08-11 16:20:35 -0500

    still

--- src/Compiler/Render.php
+++ src/Compiler/Render.php
@@ -6,9 +6,15 @@ use RuntimeException;
 
 class Render extends Processor
 {
+    private $template_path = null;
+
+    public function __construct()
+    {
+        $this->template_path = DS . Helpers::realpath(__DIR__ . '../../Templates') . DS;
+    }
 
     public function view()
     {
-
+        echo $this->template_path;
     }
 }
