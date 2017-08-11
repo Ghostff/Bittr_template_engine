220f4c08509016bfeaaa2fd2850e73ff8814ae6c
chiderah ugwu <chiderah@medology.com>
2017-08-11 16:39:20 -0500

    still

--- src/Compiler/Processor.php
+++ src/Compiler/Processor.php
@@ -6,4 +6,15 @@ namespace Compiler;
 abstract class Processor
 {
 
+    protected abstract function makeName(string $name);
+
+    protected function render(string $file)
+    {
+        $before = file_get_contents($file);
+        preg_replace_callback("/(inc|req) '(.*?)'/", function (array $matches)
+        {
+            var_dump($matches);
+        }, $before);
+
+    }
 }
--- src/Compiler/Render.php
+++ src/Compiler/Render.php
@@ -7,14 +7,37 @@ use RuntimeException;
 class Render extends Processor
 {
     private $template_path = null;
+    private $extension = null;
 
     public function __construct()
     {
-        $this->template_path = DS . Helpers::realpath(__DIR__ . '../../Templates') . DS;
+        $this->template_path = DS . Helpers::realpath(Config::$path) . DS;
+        $this->extension = Config::$extension;
     }
 
-    public function view()
+    public function view(array $files, array $data = [], bool $if_exist = false)
     {
+        foreach ($files as $file)
+        {
+            $name = $this->makeName($file);
+            if (is_readable($name))
+            {
+                $this->render($name);
+            }
+            else
+            {
+                if ($if_exist)
+                {
+                    return;
+                }
+                throw new RuntimeException();
+            }
+        }
         echo $this->template_path;
+    }
+
+    protected function makeName(string $name)
+    {
+        return $this->template_path . str_replace('.', DS, $name) . '.' .  $this->extension;
     }
 }
