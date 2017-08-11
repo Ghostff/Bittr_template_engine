5c54fbc6e2f1670c88287f37ae87817edaab005a
chiderah ugwu <chiderah@medology.com>
2017-08-11 16:45:25 -0500

    still

--- src/Compiler/Render.php
+++ src/Compiler/Render.php
@@ -30,7 +30,7 @@ class Render extends Processor
                 {
                     return;
                 }
-                throw new RuntimeException();
+                $this->exception('Template : "{0}" not found in "{1}"', $file, $name);
             }
         }
         echo $this->template_path;
@@ -39,5 +39,20 @@ class Render extends Processor
     protected function makeName(string $name)
     {
         return $this->template_path . str_replace('.', DS, $name) . '.' .  $this->extension;
+    }
+
+    private function exception(): string
+    {
+        $args = func_get_args();
+        $format = array_shift($args);
+        if ( ! empty($args))
+        {
+            foreach ($args as $keys => $replacement)
+            {
+                $format = str_replace('{' . $keys . '}', $replacement, $format);
+            }
+        }
+
+        throw new RuntimeException($format);
     }
 }
