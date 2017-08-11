a939710112d1d85fee77831af82692112832e295
chiderah ugwu <chiderah@medology.com>
2017-08-11 16:16:19 -0500

    still

--- src/Compiler/Processor.php
+++ src/Compiler/Processor.php
@@ -3,34 +3,7 @@
 namespace Compiler;
 
 
-class Processor
+abstract class Processor
 {
-    protected $last = null;
 
-    protected function tag(string $content): string
-    {
-        return $this->gets(str_replace(['{[', '[[', ']]', ']}'], ['<?php', '<?=', '?>', '?>'], $content));
-    }
-
-    protected function gets(string $content): string
-    {
-        $new =  preg_replace_callback('/(inc|req) \'(.*?)\'/', function(array $matches)
-        {
-            $replace = '';
-            foreach (explode('|', $matches[2]) as $file)
-            {
-                if ($matches[1] == 'inc')
-                {
-                    $replace .= 'include \'' .  $file. '\'; ';
-                }
-                elseif ($matches[1] == 'req')
-                {
-                    $replace .= 'require \'' .  $file. '\'; ';
-                }
-            }
-            return $replace;
-        }, $content);
-
-        return $new;
-    }
 }
