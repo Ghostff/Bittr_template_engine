18280126bcd5a30cf4f93cad50baa4c23932a670
chiderah ugwu <chiderah@medology.com>
2017-08-11 13:29:52 -0500

    still

--- src/Compiler/Render.php
+++ src/Compiler/Render.php
@@ -61,7 +61,7 @@ class Render extends Processor
             }
             else
             {
-                unlink($this->path . $name);
+                unlink($filename);
                 return false;
             }
         }
