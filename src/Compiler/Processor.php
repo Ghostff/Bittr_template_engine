47b7568150c9a4dd091bc80877a694cb49ba3f8c
chiderah ugwu <chiderah@medology.com>
2017-08-11 16:42:09 -0500

    still

--- src/Compiler/Processor.php
+++ src/Compiler/Processor.php
@@ -7,13 +7,16 @@ abstract class Processor
 {
 
     protected abstract function makeName(string $name);
+    public abstract function view(array $files, array $data = [], bool $if_exist = false);
 
     protected function render(string $file)
     {
         $before = file_get_contents($file);
         preg_replace_callback("/(inc|req) '(.*?)'/", function (array $matches)
         {
-            var_dump($matches);
+            $files = explode('|', $matches[3]);
+            $this->view($files);
+            var_dump($files);
         }, $before);
 
     }
